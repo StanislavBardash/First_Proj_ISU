95b5090f0c969ca046e8028b503e5ade2dea2bc6
Stanislav Bardash <bardashstas@gmail.com>
2023-09-06 09:38:29 +0800

    Added floor function

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ console.log("App started");
 const current_date = new Date();
 const n_y = new Date(`${current_date.getFullYear()+1}-01-01`);
 var diff = Math.abs(n_y-current_date);
-console.log("Days left before The New Year: ",diff / 1000/3600/24)
+console.log("Days left before The New Year: ",Math.floor(diff / 1000/3600/24))
